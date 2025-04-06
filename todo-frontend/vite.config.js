bfcedf3e445f949732530396fa02633032f6a752
richiemaeomandam <156775406+richiemaeomandam@users.noreply.github.com>
2025-04-05 16:37:56 -1000

    Update vite.config.js

--- todo-frontend/vite.config.js
+++ todo-frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:9000",
+        target: "https://backend-1-fvoi.onrender.com",
         changeOrigin: true,
         secure: false,
       },
